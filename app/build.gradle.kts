a20f02437b0202d6abd762806835f74583f202d8
ManuelSalcedo2770 <126840967+ManuelSalcedo2770@users.noreply.github.com>
2024-12-03 18:51:14 -0600

    Design of MisPeliculasScreen and some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.test.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -70,6 +71,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation("androidx.compose.material:material-icons-extended:1.7.5")
+    implementation(libs.coil.compose)
 
 
 }
