26b7cb8ad468278cee60f65decf737b60271c176
Andres <andyjaimerodriguez@gmail.com>
2024-12-04 23:45:43 -0600

    NavigationBar

--- app/src/main/java/com/example/reviewsapp/presentation/ui/screens/TodasPeliculasScreen.kt
+++ app/src/main/java/com/example/reviewsapp/presentation/ui/screens/TodasPeliculasScreen.kt
@@ -1,7 +1,10 @@
 package com.example.reviewsapp.presentation.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -10,14 +13,23 @@ import androidx.navigation.NavController
 
 @Composable
 fun TodasPeliculasScreen(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        // Contenido de la pantalla de todas las películas
-        androidx.compose.material3.Text(
-            text = "Todas las Películas y Reseñas",
-            color = Color.White
-        )
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }, // Agrega el Bottom Navigation
+        containerColor = Color(0xFF121212)
+    ) { paddingValues ->
+        // Puedes acceder a paddingValues para tu contenido
+        Column(modifier = Modifier.padding(paddingValues)) {
+            Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            // Contenido de la pantalla de todas las películas
+            androidx.compose.material3.Text(
+                text = "Todas las Películas y Reseñas",
+                color = Color.White
+            )
+        }
+        }
     }
+
 }
